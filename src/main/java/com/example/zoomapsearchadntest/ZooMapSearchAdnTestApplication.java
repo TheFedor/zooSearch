f278e5279dacb4fc1d77b96ed2d5a90d5fdeab04
Fedor <fedorkot1888v3.0@mail.ru>
2024-02-18 19:12:30 +0300

    Initial commit

---
+++ src/main/java/com/example/zoomapsearchadntest/ZooMapSearchAdnTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.zoomapsearchadntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooMapSearchAdnTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZooMapSearchAdnTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zoomapsearchadntest/ZooMapSearchAdnTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zoomapsearchadntest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooMapSearchAdnTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
