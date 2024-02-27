44254f6627adc61278d3979b8ec1a84c8382a999
Fedor <fedorkot1888v3.0@mail.ru>
2024-02-27 07:42:34 +0300

    Урезал функционал

--- src/main/resources/static/javaScript/scripts.js
+++ src/main/resources/static/javaScript/scripts.js
@@ -1,15 +1,3 @@
-function submitForm() {
-    var inputValue = document.getElementById('inputField').value;
-    console.log(inputValue); // Выводим значение в консоль
-    document.getElementById('inputField').style.color = '#006400'; // Устанавливаем темно-зеленый цвет текста
-    inputField.style.fontWeight = 'bold'; // Устанавливаем полужирный шрифт
-}
-
-document.getElementById('inputField').addEventListener('input', function() {
-    this.style.color = '#000'; // Когда поле начинает редактироваться, устанавливаем черный цвет текста
-    inputField.style.fontWeight = 'normal'; // Убираем полужирный шрифт после редактирования
-});
-
 function filterAnimals() {
     var input, filter, select, options, option, i, txtValue;
     input = document.getElementById('inputField');
@@ -27,14 +15,6 @@ function filterAnimals() {
         }
     }
 }
-
-function handleSelection() {
-    var select = document.getElementById('animalSelect');
-    var selectedOption = select.options[select.selectedIndex];
-    var photoLink = selectedOption.getAttribute('data-photo');
-    displayPhoto(photoLink);
-}
-
 
 function showPhoto() {
     var select = document.getElementById('animalSelect');
