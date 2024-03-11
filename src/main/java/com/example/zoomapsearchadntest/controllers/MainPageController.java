a08e7f865102fda127ea1dee38cda67e3b97e070
Fedor <fedorkot1888v3.0@mail.ru>
2024-03-11 06:07:18 +0300

    Откат. Создал контейнеры блока поиска

--- src/main/java/com/example/zoomapsearchadntest/controllers/MainPageController.java
+++ src/main/java/com/example/zoomapsearchadntest/controllers/MainPageController.java
@@ -5,7 +5,7 @@ import com.example.zoomapsearchadntest.entities.AnimalsEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
-import com.example.zoomapsearchadntest.repositoryes.animalsRepository;
+import com.example.zoomapsearchadntest.repositoryes.AnimalsRepository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +15,9 @@ import java.util.List;
 
 @Controller //Аннотация, помечающая класс ниже как контроллер Spring MVC, который обрабатывает http-запросы от клиентов
 @RequestMapping("/") //Аннотация, указывает базовый URL, который обрабатывается этим контроллером (то есть, грубо говоря, подставляет этот юрл в начало всех юрлов в классе ниже)
-public class animalsController {
-    @Autowired //Аннотация, используемая для автоматического внедрения бина (управляемый контейнером Spring объект, который является частью приложения) в другой бин. В данном случае, автоматически внедряем зависимости репозитория 'animalsRepository' в контроллер, до того как обработаются методы ниже
-    private animalsRepository animalsRepository; //'animalsRepository' представляет собой интерфейс, который предоставляет абстракцию доступа к данным для сущности 'AnimalsEntity'. То есть, мы его тут обязаны использовать, чтобы выполнять CRUD-операции с таблицей БД из этого класса
+public class MainPageController {
+    @Autowired //Аннотация, используемая для автоматического внедрения бина (управляемый контейнером Spring объект, который является частью приложения) в другой бин. В данном случае, автоматически внедряем зависимости репозитория 'animalsRepository' в контроллер, до того как обработаются методы нижеSystem.out.println("trrrrrr");
+    private AnimalsRepository animalsRepository; //'animalsRepository' представляет собой интерфейс, который предоставляет абстракцию доступа к данным для сущности 'AnimalsEntity'. То есть, мы его тут обязаны использовать, чтобы выполнять CRUD-операции с таблицей БД из этого класса
 
     @GetMapping //Аннотация, обозначающая метод, который обрабатывае HTTP GET запросы к обозначенному в скобках URL (В данном случае, т.к. в скобках ничего, берется URL из @RequestMapping)
     public String index(Model model) { //Объект Model используется для хранения и передачи данных представлению
@@ -26,6 +26,6 @@ public class animalsController {
             System.out.println(animals.get(i));
         }
         model.addAttribute("animals", animals); //Сохраняет данные animals в модель под именем "animals". Другими словами, добавляет атрибут с именем "animals" и значением animals в объект Model
-        return "animalsPage_old"; //Возвращает имя представления, которое должно быть отображено. Тут "animalsPage" указывает на имя файла представления (http-файла), который должен быть отображен пользователю в результате работы этого метода, в ответ на запрос
+        return "main_page"; //Возвращает имя представления, которое должно быть отображено. Тут "animalsPage" указывает на имя файла представления (http-файла), который должен быть отображен пользователю в результате работы этого метода, в ответ на запрос
     }
 }
--- src/main/java/com/example/zoomapsearchadntest/repositoryes/AnimalsRepository.java
+++ src/main/java/com/example/zoomapsearchadntest/repositoryes/AnimalsRepository.java
@@ -3,12 +3,10 @@ package com.example.zoomapsearchadntest.repositoryes;
 //Импорты необходимых классов и интерфейсов из других пакетов и библиотек
 import com.example.zoomapsearchadntest.entities.AnimalsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import java.awt.*;
 
 @Repository //Аннотация, обозначающая, что интерфейс ниже - репозиторий (Spring компонент, который обеспечивает доступ и методы доступа к данным таблицы animals)
 //Интерфейс animalRepository, расширяющий интерфейс JpaRepository (Предоставляет реализацию CRUD-операций для сущностей JPA, то есть обеспечивает базовую функциональность взаимодействия с данными)
-public interface animalsRepository extends JpaRepository<AnimalsEntity, Integer> { //JpaRepository<AnimalsEntity, Integer>: AnimalsEntity - сущность, с которой работает репозиторий; Integer - тип первичного ключа для этой сущности
+public interface AnimalsRepository extends JpaRepository<AnimalsEntity, Integer> { //JpaRepository<AnimalsEntity, Integer>: AnimalsEntity - сущность, с которой работает репозиторий; Integer - тип первичного ключа для этой сущности
 
 }
