baecdbfc91fa43c79ab6ff91ce5647ba07ed3d76
Fedor <fedorkot1888v3.0@mail.ru>
2024-04-10 10:38:07 +0300

    базово оформил маршрутный лист и маршрутный список

--- src/main/resources/static/javaScript/top_panel_scripts.js
+++ src/main/resources/static/javaScript/top_panel_scripts.js
@@ -40,13 +40,16 @@ function addToRouteList(name, bool) { //Функция для добавлени
 
     //рассматриваем три случая: что это первая запись маршрутного листа, вторая или большая
     //здесь пока смухлюем и решим что у всех зданий один вход
+    var name = name + '▶' + counter;
+    counter += 1;
     var routeInnerArray = [name, thisEntrances[0][0], thisEntrances[0][1]];
     if (routeList.length === 0) {
         routeList.push(routeInnerArray);
         //добавляем картинку в маршрутный лист
         addImageToTopPanel(imgSrc, name);
+        console.log('добавили: ' + routeList[routeList.length-1]);
     } else if (routeList.length === 1) {
-        routeList.push([counter, true]);
+        routeList.push([counter + '▶arrow', true]);
         routeList.push(routeInnerArray);
         //добавляем главный переход в маршрутный лист
         addArrowTranslationBetweenImagesOfRouteList(counter, true);
@@ -54,9 +57,9 @@ function addToRouteList(name, bool) { //Функция для добавлени
         //добавляем картинку в маршрутный лист
         addImageToTopPanel(imgSrc, name);
         //строим маршрут
+        console.log('добавили: ' + routeList[routeList.length-2] + '  а также: '  + routeList[routeList.length-1]);
     } else {
-        routeList.push(counter, false);
-        routeInnerArray = [name, thisEntrances[0][0], thisEntrances[0][1]];
+        routeList.push([counter + '▶arrow', false]);
         routeList.push(routeInnerArray);
         //добавляем стандартный переход в маршрутный лист
         addArrowTranslationBetweenImagesOfRouteList(counter, false);
@@ -64,18 +67,23 @@ function addToRouteList(name, bool) { //Функция для добавлени
         //добавляем картинку в маршрутный лист
         addImageToTopPanel(imgSrc, name);
         //строим маршрут
+        console.log('добавили: ' + routeList[routeList.length-2] + '  а также: '  + routeList[routeList.length-1]);
     }
+    console.log('текущий routeList:');
+    for (var i = 0; i < routeList.length; ++i) {
+        console.log(i  + ': ' + routeList[i]);
+    }
+    console.log('Размер routeList: ' + routeList.length);
+    console.log(' ');
 }
 
 function addArrowTranslationBetweenImagesOfRouteList(nameId, mainArrow) {
     var img = document.createElement('img');
-    var specialId;
+    var specialId = nameId + '▶arrow';
     if (mainArrow) { //если стрелка главного перехода
         img.src = '../photosOfRouteListTransition/Главный-переход.png';
-        specialId = nameId + '▶main';
     } else {
         img.src = '../photosOfRouteListTransition/Переход.png';
-        specialId = nameId + '▶stand';
     }
     img.style.height = '100%';
     img.style.width = 'auto';
@@ -88,7 +96,39 @@ function addArrowTranslationBetweenImagesOfRouteList(nameId, mainArrow) {
 }
 
 function makeArrowToMain(nameId) { // функция для перевода стрелки-перехода марш. листа в главную
-    console.log("стрелка с id=" + nameId + "скоро будет главной");
+    console.log('меняем главную стрелку на ' + nameId);
+    //Для начала убираем прошлую mainWay
+    console.log('ищем старую главную стрелку ');
+    for (var i = 1; i < routeList.length; i+=2) {
+        console.log(i + '- ' + routeList[i][0]);
+        if (routeList[i][1]) {
+            console.log('старая найдена: ' + routeList[i][0] + 'на индексе: '  + i);
+
+            routeList[i][1] = false;
+            var lastMainArrow = document.getElementById(routeList[i][0]);
+            if (lastMainArrow) {
+                lastMainArrow.src = '../photosOfRouteListTransition/Переход.png';
+            } else { console.log('Прошлая main стрелка не была найдена'); }
+            break;
+        }
+    }
+    //теперь делаем текущую стрелку mainWay
+    console.log('теперь ищем новую главную стрелку ' + nameId);
+    for (var i = 1; i < routeList.length; i+=2) {
+        console.log(i + '- ' + routeList[i][0]);
+        if (routeList[i][0] === nameId) {
+            console.log('новая найдена: ' + routeList[i][0] + 'на индексе: ' + i);
+            routeList[i][1] = true;
+            var newMainArrow = document.getElementById(nameId);
+            if (newMainArrow) {
+                newMainArrow.src = '../photosOfRouteListTransition/Главный-переход.png';
+            } else { console.log('Новая main стрелка ' + nameId + ' не найдена'); }
+            break;
+        }
+    }
+    console.log(' ');
+
+    //строим маршрут
 }
 
 function addImageToTopPanel(imgSrc, name) { //функция для добавления картинка в top-panel
@@ -98,7 +138,7 @@ function addImageToTopPanel(imgSrc, name) { //функция для добавл
     img.style.height = '100%';
     img.style.width = 'auto';
     img.style.objectFit = 'cover';
-    var specialId =  'img▶' + name;
+    var specialId =  name;
     img.id = specialId;
     img.onclick = function() {
         dropImageFromTopPanel(specialId);
@@ -107,5 +147,128 @@ function addImageToTopPanel(imgSrc, name) { //функция для добавл
 }
 
 function dropImageFromTopPanel(specialId) { //функция для удаления картинки из top-panel
-    console.log('решили удалить из topPanel: ' + specialId);
+    //ищем удаляемое изображение в маршрутном листе
+    var index;
+    for (var i = 0; i < routeList.length; i+= 2) {
+        if (routeList[i][0].toLowerCase() === specialId.toLowerCase()) {
+            index = i;
+            break;
+        }
+    }
+
+    //получаем важность стрелки после изображения (если она есть)
+    var mainArrowAfter = false;
+    var mainArrowBefore = false;
+    if (index + 1 < routeList.length)
+    {
+        if (routeList[index+1][1]) {
+            mainArrowAfter = true;
+        }
+    }
+    if (index - 1 >= 0) {
+        if (routeList[index-1][1]) {
+            mainArrowBefore = true;
+        }
+    }
+
+    var imageToDelete = document.getElementById(specialId);
+
+    //рассматриваем случаи: удаляют последнее изображение (просто и перед ним mainWay) / удаляют единственное изображение / удаляют изображение перед mainWay (просто и изображение первое) /удаляют стандарт
+    if (index === (routeList.length - 1)) { //первый случай
+        if (routeList.length === 1) { //второй случай
+            console.log('Путь 4');
+            if (imageToDelete) {
+                topPanel.removeChild(imageToDelete);
+                routeList.splice(index, 1);
+            } else {
+                console.log('Изображение' + specialId + 'не найдено')
+            }
+        } else { //продолжение первого случая
+            if (mainArrowBefore) { //вторая часть первого случая
+                //если всего в маршрутном листе две точки
+                if (routeList.length === 3) {
+                    console.log('Путь 2');
+                    var arrowName = routeList[index-1][0];
+                    var arrowToDelete = document.getElementById(arrowName);
+                    //удаляем картинки
+                    if (imageToDelete && arrowToDelete) {
+                        topPanel.removeChild(imageToDelete);
+                        topPanel.removeChild(arrowToDelete);
+                        //удаляем изображение и стрелку из списка
+                        routeList.splice(index-1, 2);
+                    } else { console.log('Изображение ' + imageToDelete + ' / ' + arrowToDelete + 'не найдено');}
+                } else {
+                    console.log('Путь 3');
+                    //передвигаем стрелку mainWay на один раньше
+                    makeArrowToMain(routeList[index-3][0]);
+                    //далее удаляем по стандарту
+                    var arrowName = routeList[index-1][0];
+                    var arrowToDelete = document.getElementById(arrowName);
+                    //удаляем картинки
+                    if (imageToDelete && arrowToDelete) {
+                        topPanel.removeChild(imageToDelete);
+                        topPanel.removeChild(arrowToDelete);
+                        //удаляем изображение и стрелку из списка
+                        routeList.splice(index-1, 2);
+                    } else { console.log('Изображение ' + imageToDelete + ' / ' + arrowToDelete + 'не найдено');}
+                }
+            } else { //первая часть первого случая
+                console.log('Путь 1');
+                //получаем id стрелки после для ее удаления
+                var arrowName = routeList[index-1][0];
+                var arrowToDelete = document.getElementById(arrowName);
+                //удаляем картинки
+                if (imageToDelete && arrowToDelete) {
+                    topPanel.removeChild(imageToDelete);
+                    topPanel.removeChild(arrowToDelete);
+                    //удаляем изображение и стрелку из списка
+                    routeList.splice(index-1, 2);
+                } else { console.log('Изображение ' + imageToDelete + ' / ' + arrowToDelete + 'не найдено');}
+            }
+        }
+    } else if (mainArrowAfter) {
+        if (routeList.length === 3) {
+            console.log('Путь 5');
+            //получаем id стрелки после для ее удаления
+            var arrowName = routeList[index+1][0];
+            var arrowToDelete = document.getElementById(arrowName);
+            //удаляем картинки
+            if (imageToDelete && arrowToDelete) {
+                topPanel.removeChild(imageToDelete);
+                topPanel.removeChild(arrowToDelete);
+                //удаляем изображение и стрелку из списка
+                routeList.splice(index, 2);
+            } else { console.log('Изображение ' + imageToDelete + ' / ' + arrowToDelete + 'не найдено');}
+        } else {
+            console.log('Путь 6');
+            //переводим mainWay стрелку на один вперед
+            makeArrowToMain(routeList[index+3][0])
+            //Далее удаляем по стандарту
+            //получаем id стрелки после для ее удаления
+            var arrowName = routeList[index+1][0];
+            var arrowToDelete = document.getElementById(arrowName);
+            //удаляем картинки
+            if (imageToDelete && arrowToDelete) {
+                topPanel.removeChild(imageToDelete);
+                topPanel.removeChild(arrowToDelete);
+                //удаляем изображение и стрелку из списка
+                routeList.splice(index, 2);
+            } else { console.log('Изображение ' + imageToDelete + ' / ' + arrowToDelete + 'не найдено');}
+        }
+    } else {
+        console.log('Путь 7');
+        //просто стандартно удаляем
+        //получаем id стрелки после для ее удаления
+        var arrowName = routeList[index+1][0];
+        var arrowToDelete = document.getElementById(arrowName);
+        //удаляем картинки
+        if (imageToDelete && arrowToDelete) {
+            topPanel.removeChild(imageToDelete);
+            topPanel.removeChild(arrowToDelete);
+            //удаляем изображение и стрелку из списка
+            routeList.splice(index, 2);
+        } else { console.log('Изображение ' + imageToDelete + ' / ' + arrowToDelete + 'не найдено');}
+    }
+
+    //строим маршрут
 }
