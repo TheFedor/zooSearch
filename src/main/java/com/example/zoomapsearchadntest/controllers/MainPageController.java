5abeba8e6d74e6084ff4c4a99dcbdba42784786d
Fedor <fedorkot1888v3.0@mail.ru>
2024-04-10 03:32:39 +0300

    понял как передать марицу в js

--- src/main/java/com/example/zoomapsearchadntest/controllers/MainPageController.java
+++ src/main/java/com/example/zoomapsearchadntest/controllers/MainPageController.java
@@ -3,6 +3,8 @@ package com.example.zoomapsearchadntest.controllers;
 //Импорт необходимых классов и интерфейсов из других пакетов и библиотек
 import com.example.zoomapsearchadntest.entities.*;
 import com.example.zoomapsearchadntest.repositoryes.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
@@ -32,7 +34,7 @@ public class MainPageController {
     private EntrancesToBuildingsRepository entrancesToBuildingsRepository;
 
     @GetMapping //Аннотация, обозначающая метод, который обрабатывает HTTP GET запросы к обозначенному в скобках URL (В данном случае, т.к. в скобках ничего, берется URL из @RequestMapping)
-    public String index(Model model) { //Объект Model используется для хранения и передачи данных представлению
+    public String index(Model model) throws JsonProcessingException { //Объект Model используется для хранения и передачи данных представлению
         List<AnimalsInfoEntity> animalsInfo = animalsInfoRepository.findAll();//(Sort.by(Sort.Direction.ASC, "animalsEntity.animalSpecies"));
         //На всякий сортируем полученный список
         Collections.sort(animalsInfo, new Comparator<AnimalsInfoEntity>() {
@@ -59,7 +61,7 @@ public class MainPageController {
 
         //List<AnimalsEntity> animals = animalsRepository.findAll(Sort.by(Sort.Direction.ASC, "animalSpecies")); //findAll() - Базовый метод JpaRepository для получения всех строк из таблицы БД, которые соответствуют сущности 'AnimalsEntity'; возвращает список объектов 'AnimalsEntity', содержащий все строки из таблицы БД, соответствующие этой сущности. Тут они сортируются по полю "animalSpecies" в порядке возрастания
         System.out.println("Контроллер сработал");
-        //model.addAttribute("animalsInfo", animalsInfo);
+
         model.addAttribute("structuresInfo",structuresInfo);
         model.addAttribute("animalsInfo", animalsInfo); //Сохраняет данные animals в модель под именем "animals". Другими словами, добавляет атрибут с именем "animals" и значением animals в объект Model
 
@@ -71,6 +73,15 @@ public class MainPageController {
         List<ServiceInfrastructureAvatarsEntity> serviceInfrastructureAvatarsList = serviceInfrastructureAvatarsRepository.findAll();
         model.addAttribute("animalAvatars", animalAvatarsList);
         model.addAttribute("infrastructureAvatars", serviceInfrastructureAvatarsList);
+
+        int[][] intMatr = new int[2][2];
+        intMatr[0][0] = 1;
+        intMatr[0][1] = 2;
+        intMatr[1][0] = 9;
+        intMatr[1][1] = 10;
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonIntMatr = objectMapper.writeValueAsString(intMatr);
+        model.addAttribute("intMatr", jsonIntMatr);
 
         return "main_page"; //Возвращает имя представления, которое должно быть отображено. Тут "animalsPage" указывает на имя файла представления (http-файла), который должен быть отображен пользователю в результате работы этого метода, в ответ на запрос
     }
--- src/main/resources/static/javaScript/map_scripts.js
+++ src/main/resources/static/javaScript/map_scripts.js
@@ -24,11 +24,22 @@ document.getElementById('zoom-out').addEventListener('click', function () {
 })
 
 function displayAllAvatars() {
-    var entityDataElement = document.getElementById('avatars-data');
-    var animalAvatarsList = JSON.parse(entityDataElement.getAttribute('data-animal-avatars'));
-    var structureAvatarsList = JSON.parse(entityDataElement.getAttribute('data-structure-avatars'));
+    //var entityDataElement = document.getElementById('avatars-data');
+    //var animalAvatarsList = JSON.parse(entityDataElement.getAttribute('data-animal-avatars'));
+    //var structureAvatarsList = JSON.parse(entityDataElement.getAttribute('data-structure-avatars'));
 
     console.log('размещаем аватары');
+
+    var matrElement = document.getElementById('matr');
+    var matr1 = matrElement.getAttribute('matr1');
+
+    matr1 = JSON.parse(matr1);
+    console.log('выводим матрицы:')
+    console.log(matr1[0][0]);
+    console.log(matr1[0][1]);
+    console.log(matr1[1][0]);
+    console.log(matr1[1][1]);
+    console.log('вывели матрицы.')
 
 }
 
