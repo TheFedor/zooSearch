39966bd0d75fec0e39626db69798148af935d301
Fedor <fedorkot1888v3.0@mail.ru>
2024-04-10 06:14:04 +0300

    расположил все аватарки на карте

--- src/main/resources/static/javaScript/map_scripts.js
+++ src/main/resources/static/javaScript/map_scripts.js
@@ -48,18 +48,67 @@ function gettingLocationsAndCoordinatesOfEntrances() {
 gettingLocationsAndCoordinatesOfEntrances();
 
 function displayAllAvatars() {
-    console.log('Полученные данные по расположению животных');
+
+    //проходим по массиву с аватарками животных
+    var uniqueElements = {};
     for (var i = 0; i < locationOfAnimals.length; ++i) {
-        console.log(i + ': ' + locationOfAnimals[i][0] + ' ; ' + locationOfAnimals[i][1] + ' ; ' + locationOfAnimals[i][2] + ' ; ' + locationOfAnimals[i][3] + ' ; ' + locationOfAnimals[i][4] + ' ; ' + locationOfAnimals[i][5]);
+        console.log('размещаем аватар: ' + locationOfAnimals[i][0]);
+        if (locationOfAnimals[i][2].toLowerCase() === 'null') { //размещаем аватарку животного
+            console.log('    отдельная зона');
+            var img = document.createElement('img'); //создаем новый элемент - изображение
+            img.src = locationOfAnimals[i][1]; //устанавливаем путь к изображению
+            img.style.position = 'absolute'; //устанавливаем абсолютное позиционирование
+            //устанавливаем размеры изображения
+            img.width = 40;
+            img.height = 40;
+            //устанавливаем координаты изображения относительно блока map-container
+            var xCoordinate = parseInt(locationOfAnimals[i][3].trim());
+            var yCoordinate = parseInt(locationOfAnimals[i][4].trim());
+            img.style.left = (xCoordinate/8) + 'px';
+            img.style.top = (yCoordinate/8) + 'px';
+            //устанавливаем id изображения
+            img.id = locationOfAnimals[i][0] + '▶' + locationOfAnimals[i][5];
+            //добавляем изображение в родительский элемент
+            mapContainer.appendChild(img);
+        } else { //размещаем аватарку постройки, если еще не была размещена
+            console.log('    общее здание')
+            if (!uniqueElements[locationOfAnimals[i][2]]) { //только если ранее не размещали эту аватарку
+                uniqueElements[locationOfAnimals[i][2]] = true;
+                var img = document.createElement('img'); //создаем новый элемент - изображение
+                img.src = locationOfAnimals[i][2]; //устанавливаем путь к изображению
+                img.style.position = 'absolute'; //устанавливаем абсолютное позиционирование
+                //устанавливаем размеры изображения
+                img.width = 40;
+                img.height = 40;
+                //устанавливаем координаты изображения относительно блока map-container
+                var xCoordinate = parseInt(locationOfAnimals[i][3].trim());
+                var yCoordinate = parseInt(locationOfAnimals[i][4].trim());
+                img.style.left = (xCoordinate/8) + 'px';
+                img.style.top = (yCoordinate/8) + 'px';
+                //устанавливаем id изображения
+                img.id = locationOfAnimals[i][0] + '▶' + locationOfAnimals[i][5];
+                //добавляем изображение в родительский элемент
+                mapContainer.appendChild(img);
+            }
+        }
     }
-    console.log('Полученные данные по структурам')
+
+    //проходим по массиву с аватарками структур
     for (var i = 0; i < locationOfStructures.length; ++i) {
-        console.log(i + ': ' + locationOfStructures[i][0] + ' ; ' + locationOfStructures[i][1] + ' ; ' + locationOfStructures[i][2] + ' ; ' + locationOfStructures[i][3] + ' ; ' + locationOfStructures[i][4]);
+        console.log('размещаем структуры');
+        var img = document.createElement('img');
+        img.src = locationOfStructures[i][1];
+        img.style.position = 'absolute';
+        img.width = 40;
+        img.height = 40;
+        var xCoordinate = parseInt(locationOfStructures[i][2].trim());
+        var yCoordinate = parseInt(locationOfStructures[i][3].trim());
+        img.style.left = (xCoordinate/8) + 'px';
+        img.style.top = (yCoordinate/8) + 'px';
+        img.id = locationOfStructures[i][0] + '▶' + locationOfStructures[i][4];
+        mapContainer.appendChild(img);
     }
-    console.log('Данные по входам')
-    for (var i = 0; i < entrances.length; ++i) {
-        console.log(i + ": " + entrances[i][0] + ' ; ' + entrances[i][1] + ' ; ' + entrances[i][2]);
-    }
+
 }
 
 //сразу вызываем функцию
